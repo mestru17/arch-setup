3c4a9251fca0ca052705a0200664030814b4ce6a
Mathias Estrup <32955302+mestru17@users.noreply.github.com>
2022-08-01 12:41:23 +0200

    Fix lightdm resolution

---
+++ root/etc/lightdm/lightdm-display-setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+# This is not an official ligthdm config file.
+# It is a custom display setup script that is set to run automatically by /etc/lightdm/lightdm.conf in my configuration.
+xrandr -s 1920x1080
